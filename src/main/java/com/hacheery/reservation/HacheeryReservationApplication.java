f7bcb15489d89b40c14638f601b44005638862d7
hachnv8 <hacnv8@gmail.com>
2023-03-02 21:31:52 +0700

    initialize project

---
+++ src/main/java/com/hacheery/reservation/HacheeryReservationApplication.java
@@ -0,0 +1,13 @@
+package com.hacheery.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacheeryReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HacheeryReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hacheery/reservation/HacheeryReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacheery.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacheeryReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
